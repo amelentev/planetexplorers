985c6ab4c507212f87037a9362b1c709bd3cfaf5
Artem Melentev <artem@melentyev.me>
2023-01-22 19:49:44 +0100

    fix intro scene for unity2019

--- Assets/Resources/Movie/IntroRunner.cs
+++ Assets/Resources/Movie/IntroRunner.cs
@@ -4,33 +4,18 @@ using UnityEngine.UI;
 
 public class IntroRunner : MonoBehaviour
 {
-    MovieTexture mMovie;
-
-    void Start()
-    {
-        mMovie = GetComponent<RawImage>().texture as MovieTexture;
-		if (mMovie != null) {
-			mMovie.Stop ();
-			mMovie.Play ();
-
-			AudioSource sound = gameObject.AddComponent<AudioSource>();
-			sound.clip = mMovie.audioClip;
-			sound.Stop();
-			sound.Play();
-		}
+    void Start() {
+        var videoPlayer = GetComponent<UnityEngine.Video.VideoPlayer>();
+        if (videoPlayer) {
+            videoPlayer.loopPointReached += ((vp) => EndMovie());
+        }
     }
 
-    void Update()
-    {
-		if (mMovie != null && !mMovie.isPlaying)
-        {
-            EndMovie();
-        }
-
+    void Update() {
         if (Input.GetKeyDown(KeyCode.Escape)
             || Input.GetKeyDown(KeyCode.Return)
             || Input.GetKeyDown(KeyCode.Space)
-            || Input.GetMouseButtonDown(0)) //lz-2016.08.16  功能 #3111片头动画在现有按键跳过基础上增加鼠标左键跳过
+            || Input.GetMouseButtonDown(0))
         {
             EndMovie();
         }
